46189cd79737189c53d8202e427020dcbfa4b2d6
EmilioOcelotl <emilio.ocelotl@gmail.com>
2025-09-10 18:49:03 -0600

    application global en telegram_bot para simplificar send_message

--- bot/telegram_bot.py
+++ bot/telegram_bot.py
@@ -3,6 +3,9 @@ from telegram.ext import Application, MessageHandler, filters, ContextTypes
 from .config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
 from .reminder import add_reminder
 
+# Creamos la aplicación global del bot
+application = Application.builder().token(TELEGRAM_TOKEN).build()
+
 # Para recibir y guardar recordatorios desde Telegram
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text.strip()
@@ -12,6 +15,6 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
         add_reminder(contenido)
         await update.message.reply_text("✅ Recordatorio agregado.")
 
-# Función para enviar mensajes desde otros scripts
-async def send_message(text, application):
+# Función para enviar mensajes desde otros scripts (no necesita application como parámetro)
+async def send_message(text: str):
     await application.bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=text)
